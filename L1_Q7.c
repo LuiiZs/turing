49433acf3a75e33e216a27b6c560cfad898d32ec
Herbert Lucas <hlcf.eng20@uea.edu.br>
2022-08-30 17:46:15 -0300

    Questao 7 da lista 1 - Herbert Lucas

--- L1_Q7.c
+++ L1_Q7.c
@@ -4,30 +4,43 @@ Carlos Rodrigo Santos da Silva - 2015090002
 Herbert Lucas Corrêa Fonseca - 2015090044
 */
 
-#include<stdio.h>
+#include <stdio.h>
 
-int main(argc, char** argv)
+int main()
 {
-    
-    int n, qtdp=0, qtdi=0, i=0;
-    
-    while(i<10)
-    {
-        printf("Digite um numero: ");
-        scanf("%d", &n);
-        
-        if(n%2==0)
-        {
-            qtdp++;
-        }
-        else
-        {
-            qtdi++;
-        }
-        i++;
-    }
-    
-    printf("Quantidade de Pares: %d\n", qtdp);
-    printf("Quantidade de Impares: %d\n", qtdi);
-    return 0;
+	float ap1, ap2, m, pf;
+	{
+		printf("Digite a nota da sua AP1:\n");
+		scanf("%f", &ap1);
+
+		printf("Digite a nota da sua AP2:\n");
+		scanf("%f", &ap2);
+
+		m=(ap1+ap2)/2;
+	}
+	if(m>=8)
+	{
+		printf("Voçê está aprovado com nota = %.2f\n", m);
+	}
+	else if (m>=4 && m<8)
+	{
+		printf("Digite a nota da sua PF:\n");
+		scanf("%f", &pf);
+
+		m=(m+pf)/2;
+
+		if(m>=6)
+		{
+			printf("Voçê está aprovado com nota = %.2f\n", m);
+		}
+		else
+		{
+			printf("Voçê está reprovado");
+		}
+	}
+	else
+	{
+		printf("Voçê está reprovado direto\n");
+	}
+	return 0;
 }
