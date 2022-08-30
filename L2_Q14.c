c102191500ac82105d33ec8e27d0c3197905f881
Carlos rodrigo santos da silva <zcarlosrodrigo@githun.com>
2022-08-30 18:38:24 -0300

    questão 14 da lista 2 - C.R

--- L2_Q14.c
+++ L2_Q14.c
@@ -4,17 +4,17 @@
 
 int main (int argc, char** argv)
 {
-	float h, i=1, n, soma=0, cima=1;
-	{
-		printf("Quantos termos voçê quer que some:\n");
-		scanf("%f", &n);
-	}
-	while (n>=i)
-	{
-		h=cima/i;
-		soma=soma+h;
-		i++;
-	}
-	printf("O valor de h é:%.2f\n", soma);
-	return 0;
+        float h, i=1, n, soma=0, cima=1;
+        {
+                printf("Quantos termos voçê quer que some:\n");
+                scanf("%f", &n);
+        }
+        while (n>=i)
+        {
+                h=cima/i;
+                soma=soma+h;
+                i++;
+        }
+        printf("O valor de h é:%.2f\n", soma);
+        return 0;
 }
